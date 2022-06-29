49a9358e737286926af056cdad321f072b1403c5
Błażej Sowa <bsowa123@gmail.com>
2022-06-29 09:58:39 +0200

    Refactor (#2)
    * Fix package name
    
    * Make the script executable
    
    * Add pylibftdi to dependencies
    
    * Move the python package to src directory
    
    * Fix command in README
    
    * Add ftdi library to dependencies
    
    * Fix python3 compatibility
    
    * Use private names for services
    
    * Change service type definitions
    
    * Fix state array lenght in get callback
    
    * Check for exception in set_callback
    
    * Spawn services after initialization, cosmetic changes

--- scripts/relay_node.py
+++ scripts/relay_node.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
-from tokenize import String
 import rospy
 import time
 
-import argparse
-
-from dae_relay_controller_ros.srv import *
+from dae_relay_controller_ros.srv import (
+    SetRelay,
+    GetAllRelays,
+    SetAllRelays,
+    SetRelayResponse,
+    GetAllRelaysResponse,
+    SetAllRelaysResponse,
+)
 import dae_RelayBoard
+from dae_RelayBoard.dae_RelayBoard_Common import Denkovi_Exception
 
 
 def set_callback(msg):
-    dr.setState(msg.relay_number, msg.state)
+    try:
+        dr.setState(msg.relay_number, msg.state)
+    except Denkovi_Exception as e:
+        rospy.logerr(f"Failed to set relay state: {e}")
+        return SetRelayResponse(False)
+
     time.sleep(0.5)
     resp = dr.getStates()
 
@@ -20,17 +30,17 @@ def set_callback(msg):
     else:
         is_ok = False
 
-    return SetRelayResponse(is_ok, "")
+    return SetRelayResponse(is_ok)
 
 
 def get_callback(msg):
     state = dr.getStates()
-    state_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    state_arr = [False] * len(state)
 
     for tmp in state:
         state_arr[tmp - 1] = state[tmp]
 
-    return GetRelayResponse(state_arr, "")
+    return GetAllRelaysResponse(True, state_arr)
 
 
 def set_all_callback(msg):
@@ -38,37 +48,30 @@ def set_all_callback(msg):
     dr.setAllStatesOn() if msg.state else dr.setAllStatesOff()
 
     resp = dr.getStates()
-    print(resp)
 
     for tmp in resp:
         if resp[tmp] != msg.state:
             is_ok = False
 
-    return SetAllRelaysResponse(is_ok, "")
+    return SetAllRelaysResponse(is_ok)
 
 
-try:
-    rospy.init_node("relay_node")
-
-    set_relay_srv = rospy.Service("relay/set_relay", SetRelay, set_callback)
-    set_all_relays = rospy.Service("relay/get_relay", GetRelay, get_callback)
-    get_relay = rospy.Service("relay/set_relay/all", SetAllRelays, set_all_callback)
-
-    rospy.loginfo("Relay node started!")
-
-except rospy.ROSInterruptException as e:
-    rospy.loginfo("Relay node error!")
-    rospy.logerr(e)
+rospy.init_node("relay_node")
 
 device = rospy.get_param("~device", "/dev/relay")
 type = rospy.get_param("~module_type", "type16")
 
 dr = dae_RelayBoard.DAE_RelayBoard(type)
 dr.initialise(device)
+dr.setAllStatesOff()
 
 rospy.loginfo("Device type: %s", type)
-rospy.loginfo("Device path: %s", device)
+rospy.loginfo("Device path/id: %s", device)
 
-dr.setAllStatesOff()
+set_relay_srv = rospy.Service("~set_relay", SetRelay, set_callback)
+set_all_relays_srv = rospy.Service("~get_all_relays", GetAllRelays, get_callback)
+get_all_relays_srv = rospy.Service("~set_all_relays", SetAllRelays, set_all_callback)
+
+rospy.loginfo("Relay node started!")
 
 rospy.spin()
--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=["dae_relay_controller_ros"], package_dir={"": "lib"})
+setup_args = generate_distutils_setup(
+    packages=["dae_RelayBoard"], package_dir={"": "src"}
+)
 
 setup(**setup_args)
